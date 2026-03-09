bb0be4f312ee7f86dfd1acf9051026c67a4645a6
vfa59 <vittorioang9898@gmail.com>
2026-03-10 00:47:14 +0800

    added urls for registration and logout

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+   path('register/', views.register, name='register'),
+   path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+]
