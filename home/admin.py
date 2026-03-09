19a622e62ccaf302899643edbd28fd131a87a924
perimeterr <mikaela.paderna@student.ateneo.edu>
2026-03-09 22:15:59 +0800

    changed difficulty logic to get text from database

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import TestText
 
 # Register your models here.
+class TestTextAdmin(admin.ModelAdmin):
+    model = TestText
+
+admin.site.register(TestText, TestTextAdmin)
