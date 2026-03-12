c942598adbdde12a6c83194f426b962a1e29c175
JenuelTenorio <jenuel.tenorio@student.ateneo.edu>
2026-03-12 19:05:49 +0800

    register Test, TestText, and TestResult in admin so we can see saved data

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-from .models import TestText
+from .models import TestText, Test, TestResult
 
-# Register your models here.
+
 class TestTextAdmin(admin.ModelAdmin):
     model = TestText
+    list_display = ('id', 'difficulty', 'content')
+
+
+class TestAdmin(admin.ModelAdmin):
+    model = Test
+    list_display = ('id', 'text', 'duration_seconds', 'created_at')
+
+
+class TestResultAdmin(admin.ModelAdmin):
+    model = TestResult
+    list_display = ('id', 'user', 'test', 'wpm', 'accuracy', 'date_taken')
+
 
 admin.site.register(TestText, TestTextAdmin)
+admin.site.register(Test, TestAdmin)
+admin.site.register(TestResult, TestResultAdmin)
