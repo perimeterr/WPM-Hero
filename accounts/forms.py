cd6f5e352c358fd1c7713ffd8337e91a2bb58a5b
glob-blob <gabriel.pacardo@student.ateneo.edu>
2026-03-13 02:19:50 +0800

    implemented login functionality

--- accounts/forms.py
+++ accounts/forms.py
@@ -10,3 +10,7 @@ class UserRegistrationForm(UserCreationForm):
    class Meta:
        model = User
        fields = ('username', 'email', 'password1', 'password2')
+
+class UserLoginForm(forms.Form):
+   username = forms.CharField(max_length=150)
+   password = forms.CharField(widget=forms.PasswordInput)
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from .forms import UserRegistrationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from .forms import UserLoginForm, UserRegistrationForm
 
 
 def register(request):
@@ -13,3 +14,25 @@ def register(request):
    else:
        form = UserRegistrationForm()
    return render(request, 'accounts/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                next_url = request.GET.get('next') or 'home:home'
+                return redirect(next_url)
+            else:
+                form.add_error(None, 'Invalid username or password')
+    else:
+        form = UserLoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('accounts:login')
