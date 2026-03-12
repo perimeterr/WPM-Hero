61c1e2c3001222a4b5ded631350df6a56d4be0cc
JenuelTenorio <jenuel.tenorio@student.ateneo.edu>
2026-03-12 19:05:18 +0800

    add save_result endpoint to store typing test results

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import home, results
+from .views import home, results, save_result
 
 urlpatterns = [
     path('', home, name='home'),
     path('results/', results, name='results'),
+    path('save-result/', save_result, name='save_result'),
 ]
 
 app_name = 'home'
