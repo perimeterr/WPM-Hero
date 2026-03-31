ff3f42c74be7f452bae2c516a188d79b883b37ca
perimeterr <mikaela.paderna@student.ateneo.edu>
2026-03-31 14:58:51 +0800

    created view for profile dashboard containing basic statistics

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.db.models import Sum
 from .forms import UserLoginForm, UserRegistrationForm, UserUpdateForm
+from home.models import TestResult
 
 
 def register(request):
@@ -45,8 +47,26 @@ def edit_profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, f'Your account has been updated!')
-            return redirect('accounts:edit_profile') 
+            return redirect('accounts:profile_dashboard') 
     else:
         form = UserUpdateForm(instance=request.user)
 
     return render(request, 'accounts/edit_profile.html', {'form': form})
+
+@login_required
+def profile_dashboard(request):
+    test_results = TestResult.objects.filter(user=request.user)
+    tests_count = test_results.count()
+
+    time_data = TestResult.objects.filter(user=request.user).aggregate(
+        total_time=Sum('test__duration_seconds')
+    )
+
+    actual_seconds = time_data['total_time'] or 0
+
+    ctx = {
+        'tests_count': tests_count,
+        'total_time': actual_seconds
+    }
+
+    return render(request, 'accounts/profile_dashboard.html', ctx)
