c431876c5ae097cdd9dd79979ef6355eb40ba5a6
perimeterr <mikaela.paderna@student.ateneo.edu>
2026-04-22 11:44:06 +0800

    fixed allowed hosts

--- wpm_hero/settings.py
+++ wpm_hero/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
