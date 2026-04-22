dce012b98fb08bb79f0b752176b7b2f3570d095d
perimeterr <mikaela.paderna@student.ateneo.edu>
2026-04-22 11:16:10 +0800

    debugging static

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 set -o errexit
 
 pip install -r requirements.txt
+python -c "import django; import os; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings'); django.setup(); from django.conf import settings; print('STATIC_ROOT:', settings.STATIC_ROOT)"
 python manage.py collectstatic --no-input
 python manage.py migrate
--- wpm_hero/settings.py
+++ wpm_hero/settings.py
@@ -127,4 +127,4 @@ STATICFILES_DIRS = [
     BASE_DIR/'static',
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
