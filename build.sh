f80ea2785be2563ce55d2c049a98b3a410809ea2
perimeterr <mikaela.paderna@student.ateneo.edu>
2026-04-22 11:21:36 +0800

    more debugging

--- build.sh
+++ build.sh
@@ -2,6 +2,17 @@
 set -o errexit
 
 pip install -r requirements.txt
-python -c "import django; import os; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings'); django.setup(); from django.conf import settings; print('STATIC_ROOT:', settings.STATIC_ROOT)"
+python -c "
+import os, sys
+sys.path.insert(0, '.')
+settings_module = os.environ.get('DJANGO_SETTINGS_MODULE', 'not set')
+print('Settings module:', settings_module)
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+django.setup()
+from django.conf import settings
+print('STATIC_ROOT:', repr(settings.STATIC_ROOT))
+print('BASE_DIR:', repr(getattr(settings, 'BASE_DIR', 'NOT FOUND')))
+"
 python manage.py collectstatic --no-input
 python manage.py migrate
