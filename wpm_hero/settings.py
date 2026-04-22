a72cb0bbd1e318c7bad75313c2686a4bd4519fe1
perimeterr <mikaela.paderna@student.ateneo.edu>
2026-04-22 11:20:24 +0800

    static root fix

--- wpm_hero/settings.py
+++ wpm_hero/settings.py
@@ -123,8 +123,9 @@ LOGOUT_REDIRECT_URL = 'home:home'
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     BASE_DIR/'static',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
